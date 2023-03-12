79870ce45e69bfe387a2af45cd50eab22ea85197
duguyumei <2670924797@qq.com>
2023-03-12 11:56:39 +0800

    element-plus 和sass

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+  <el-button>123</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VueHome',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+  .el-button{
+    font-size: 50px;
+  }
+</style>
